38d34b01d2bc46c66f2c47f8a0086cc20d4e8155
LAST2964 <drakes0214@gmail.com>
2023-10-02 23:36:27 +0000

    saving snake

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -87,11 +87,17 @@ function checkForNewDirection(event) {
 
   if (activeKey === KEY.LEFT) {
     snake.head.direction = "left";
+  } else if (activeKey === KEY.RIGHT) {
+    snake.head.direction = "right";
+  } else if (activeKey === KEY.UP) {
+    snake.head.direction = "up";
+  } else if (activeKey === KEY.DOWN) {
+    snake.head.direction = "down";
   }
 
   // FILL IN THE REST
 
-  // console.log(snake.head.direction);     // uncomment me!
+  //console.log(snake.head.direction);     // uncomment me!
 }
 
 function moveSnake() {
