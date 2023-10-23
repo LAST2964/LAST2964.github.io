dc4db748c572bfcc7ab7403de38fc3534d8f8bd0
LAST2964 <drakes0214@gmail.com>
2023-10-23 22:59:46 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,19 +10,24 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+  };
   // Game Item Objects
-
+var walker = {
+  posX: 0,
+  posY: 0, 
+  speedX: 0,
+  speedY: 0
+};
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeydown);                           // change 'eventType' to the type of event you want to handle
-var KEY = {
-  UP: 38,
-  DOWN: 40,
-  LEFT: 37,
-  RIGHT: 39,
-};
+  $(document).on('keyup', handleKeyUp);
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -32,7 +37,8 @@ var KEY = {
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+    repositionGameItem();
+    redrawGameItem();
 
   }
   
@@ -42,20 +48,33 @@ var KEY = {
   function handleKeydown(event) {
 if (event.which === KEY.UP) {
   console.log("up pressed");
+  walker.speedY = -5;
 } else if (event.which === KEY.DOWN) {
   console.log("down pressed");
+  walker.speedY = 5;
 } else if (event.which === KEY.LEFT) {
   console.log("left pressed");
+  walker.speedX = -5;
 } else if (event.which === KEY.RIGHT) {
   console.log("right pressed");
+  walker.speedX = 5;
 }
   }
-
+function handleKeyUp() {
+  walker.speedX = 0;
+  walker.speedY = 0;
+}
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
-  
+function repositionGameItem() {
+  walker.posX += walker.speedX;
+  walker.posY += walker.speedY;
+}
+function redrawGameItem() {
+  $("#walker").css("left", walker.posX);
+  $("#walker").css("top", walker.posY);
+}
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
